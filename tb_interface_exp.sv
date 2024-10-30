447afce3b2befac1af1ac128cf6108ea7812d6bf
Faraz Imran <farazimran12@gmail.com>
2024-10-30 12:32:56 -0400

    simplify tb

--- tb_interface_exp.sv
+++ tb_interface_exp.sv
@@ -2,13 +2,9 @@ module tb_interface_exp #(
   parameter int MUX_0_DATA_WITH = 8,
   parameter int MUX_1_DATA_WITH = 4
 ) (
-  input logic [MUX_0_DATA_WITH-1:0] i_a_0,
-  input logic [MUX_0_DATA_WITH-1:0] i_b_0,
-  input logic                       i_sel_0,
-
-  input logic [MUX_1_DATA_WITH-1:0] i_a_1,
-  input logic [MUX_1_DATA_WITH-1:0] i_b_1,
-  input logic                       i_sel_1,
+  input logic [MUX_0_DATA_WITH-1:0] i_a,
+  input logic [MUX_0_DATA_WITH-1:0] i_b,
+  input logic                       i_sel,
 
   output logic [MUX_0_DATA_WITH-1:0] o_y_0,
   output logic [MUX_1_DATA_WITH-1:0] o_y_1
@@ -20,14 +16,14 @@ module tb_interface_exp #(
   mux2to1 mux0 (.slave_if(mux0_if));
   mux2to1 mux1 (.slave_if(mux1_if));
 
-  assign mux0_if.i_a   = i_a_0;
-  assign mux0_if.i_b   = i_b_0;
-  assign mux0_if.i_sel = i_sel_0;
+  assign mux0_if.i_a   = i_a;
+  assign mux0_if.i_b   = i_b;
+  assign mux0_if.i_sel = i_sel;
   assign o_y_0         = mux0_if.o_y;
 
-  assign mux1_if.i_a   = i_a_1;
-  assign mux1_if.i_b   = i_b_1;
-  assign mux1_if.i_sel = i_sel_1;
+  assign mux1_if.i_a   = ~i_a[MUX_1_DATA_WITH-1:0];
+  assign mux1_if.i_b   = ~i_b[MUX_1_DATA_WITH-1:0];
+  assign mux1_if.i_sel = ~i_sel;
   assign o_y_1         = mux1_if.o_y;
 
   initial begin
