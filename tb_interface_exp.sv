34cce4dde9f95a541bd77293db71cf6a920304fb
Faraz Imran <farazimran12@gmail.com>
2024-10-30 12:52:12 -0400

    hook up interface correctly

--- tb_interface_exp.sv
+++ tb_interface_exp.sv
@@ -16,15 +16,25 @@ module tb_interface_exp #(
   mux2to1 mux0 (.slave_if(mux0_if));
   mux2to1 mux1 (.slave_if(mux1_if));
 
-  assign mux0_if.i_a   = i_a;
-  assign mux0_if.i_b   = i_b;
-  assign mux0_if.i_sel = i_sel;
-  assign o_y_0         = mux0_if.o_y;
+  if_master #(
+    .DATA_WITH(MUX_0_DATA_WITH)
+  ) master0 (
+    .i_a      (i_a),
+    .i_b      (i_b),
+    .i_sel    (i_sel),
+    .o_y      (o_y_0),
+    .master_if(mux0_if)
+  );
 
-  assign mux1_if.i_a   = ~i_a[MUX_1_DATA_WITH-1:0];
-  assign mux1_if.i_b   = ~i_b[MUX_1_DATA_WITH-1:0];
-  assign mux1_if.i_sel = ~i_sel;
-  assign o_y_1         = mux1_if.o_y;
+  if_master #(
+    .DATA_WITH(MUX_1_DATA_WITH)
+  ) master1 (
+    .i_a      (i_a[MUX_1_DATA_WITH-1:0]),
+    .i_b      (i_b[MUX_1_DATA_WITH-1:0]),
+    .i_sel    (i_sel),
+    .o_y      (o_y_1),
+    .master_if(mux1_if)
+  );
 
   initial begin
     $dumpfile("waves.vcd");
